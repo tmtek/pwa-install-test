3fe9c2480ff4138acea3ffa1a0fd9d021cda3c1a
Tony MacDonell <tony@teknision.com>
2019-03-29 16:50:08 -0400

    first pass HOC

--- src/components/app.js
+++ src/components/app.js
@@ -1,40 +1,15 @@
 import { h, Component } from 'preact';
+import PWAInstaller from '../install'
 
+@PWAInstaller()
 export default class App extends Component {
 
-	componentDidMount() {
-		window && window.addEventListener('beforeinstallprompt', (e) => {
-			e.preventDefault();
-			this.setState({ deferredInstallPrompt: e });
-		});
-
-		window.addEventListener('appinstalled', (evt) => {
-			this.setState({ wasInstalled: true });
-		});
-
-		if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true) {
-			this.setState({ runningLocally: true });
-		}
-	}
-
-	handleInstallClick = () => {
-		let { deferredInstallPrompt } = this.state;
-		if (deferredInstallPrompt) {
-			deferredInstallPrompt.prompt();
-			deferredInstallPrompt.userChoice.then((choiceResult) => {
-				let message = choiceResult.outcome === 'accepted' ? 'User accepted the A2HS prompt' : 'User dismissed the A2HS prompt';
-				this.setState({ deferredInstallPrompt:null, message });
-			});
-		}
-	}
-
-	render({},{ deferredInstallPrompt , message, wasInstalled, runningLocally}) {
+	render({isNative, installPrompt}) {
 		return (
 			<div id="app">
 				<h1>Install Test</h1>
-				{ deferredInstallPrompt && <a href="#" onClick={this.handleInstallClick}>Do Install</a> || <p>{message || 'Not yet able to install'}</p>}
-				<p>{wasInstalled && 'App is installed' || 'App is not installed'}</p>
-				<p>{runningLocally && 'App is running locally' || 'App is not running locally'}</p>
+				{ installPrompt && <a href="#" onClick={installPrompt}>Do Install</a> || <p>Nothing to install</p>}
+				<p>{ isNative && 'App is running locally' || 'App is not running locally' }</p>
 			</div>
 		);
 	}
---
+++ src/install.js
@@ -0,0 +1,76 @@
+import { h, Component } from 'preact';
+
+function isNative() {
+    return window && (
+        window.matchMedia('(display-mode: standalone)').matches || 
+        window.navigator.standalone === true
+    );
+}
+
+function listen(handlePrompt) {
+    if (!window) return null;
+    let installPrompt;
+    let installpromptListener;
+    let appInstalledListener;
+
+    let unlisten = () => installpromptListener && window.removeEventListener(installpromptListener);
+    installpromptListener = window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault();
+        installPrompt = e;
+        prompt = () =>  new Promise((resolve, reject) => {
+            installPrompt.prompt();
+			installPrompt.userChoice.then((choiceResult) => {
+				choiceResult.outcome !== 'accepted' && resolve(false);
+            });
+            appInstalledListener = window.addEventListener('appinstalled', () => {
+                resolve(true);
+            });
+        }).then(success => {
+            installPrompt = null;
+            appInstalledListener && window.removeEventListener(appInstalledListener);
+            return success;
+        });
+        handlePrompt({prompt, unlisten});
+    });
+
+    return unlisten;
+
+}
+
+export default function () {
+    return Child => {
+        class PWAInstaller extends Component {
+
+            unlistentoken = null;
+        
+            unlisten() {
+                this.setState({ prompt:null });
+                this.unlistentoken && this.unlistentoken();
+            }
+        
+            componentDidMount() {
+                let isN = isNative();
+                this.setState({ isNative:isN });
+                if(!isN) {
+                    this.unlistentoken = listen(({ prompt }) => {
+                        this.setState({ prompt: () => 
+                            prompt().then(installed => {
+                                installed && unlisten();
+                                return installed;
+                            })
+                        });  
+                    });
+                }
+            }
+            
+            componentWillUnmount() {
+                unlisten();
+            }
+        
+            render(props, { isNative, prompt }) {
+                return h(Child, {...props, isNative, installPrompt:prompt} );
+            }
+        }
+        return PWAInstaller;
+    }
+}
