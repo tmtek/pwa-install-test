5baf9056b3fff918806aa105b181b340f5b2bc7c
Tony MacDonell <tony@teknision.com>
2019-03-30 08:51:31 -0400

    documented and touched up install.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,12 @@
 import { h, Component } from 'preact';
 import installer from '../install'
 
-function App({ isNative, installPrompt }) {
+function App({ isStandalone, installPrompt }) {
 	return (
 		<div id="app">
 			<h1>Install Test</h1>
 			{ installPrompt && <a href="#" onClick={installPrompt}>Do Install</a> || <p>Nothing to install</p>}
-			<p>{ isNative && 'App is running locally' || 'App is not running locally' }</p>
+			<p>{ isStandalone && 'App is running locally' || 'App is not running locally' }</p>
 		</div>
 	);
 }
